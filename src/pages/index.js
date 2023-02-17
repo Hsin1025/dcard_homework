2d7e1be518d8bb4d0a06596c8a95c58c31b48bdd
林芯鈴 <116002213+Hsin1025@users.noreply.github.com>
2023-02-17 19:45:22 +0800

    clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,115 @@
-import Head from 'next/head'
-import Header from '../components/Header.js'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { useSession } from 'next-auth/react'
+import Head from 'next/head';
+import Link from 'next/link.js';
+import Header from '../components/Header';
+import Modal from '../components/Modal';
+import ClientOnly from '../components/ClientOnly.js';
+import SearchBar from '../components/SearchBar';
+import React from 'react';
 
-import { ApolloClient, createHttpLink, InMemoryCache, gql } from '@apollo/client'
-import { setContext } from '@apollo/client/link/context';
+import { useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import { useSession } from 'next-auth/react';
 
-export default function Home({ repository_issues }) {
+import { gql } from '@apollo/client';
+import { getApolloClient } from '../../apollo-client.js';
 
-  const { data: session, status } = useSession()
-  const loading = status === 'loading'
+const SEARCH_TASK = gql`
+query searchTask($query: String!, $first: Int!) {
+  search(type: ISSUE, first: $first, query: $query) {
+    edges {
+      node {
+        ... on Issue {
+          id
+          body
+          title
+          state
+          number
+          labels(first: 1) {
+            edges {
+              node {
+                id
+                name
+                color
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+const ISSUE_TOTAL_COUNT = gql`
+query issueTotalCount($query: String!) {
+  search(type: ISSUE, query: $query) {
+    issueCount
+  }
+}
+`
+export default function Home() {
+  const { data: session, status } = useSession();
+  const loading = status === 'loading';
+
+  const client = getApolloClient();
+
+  let modal = 'create';
+  let [isOpen, setIsOpen] = useState(false);
+
+  let[data, setData] = useState([]);
+
+  const [searchData, setSearchData] = useState({
+    labels: "",
+    body: "",
+  });
+
+  const [hasMore, setHasMore] = useState(true);
+
+  const getMoreData = async () => {
+    const labels = searchData.labels ? "label:" + searchData.labels : '';
+    const query = "repo:Hsin1025/dcard_homework " + labels + " in:body " + searchData.body
+    try{
+      var searchResult = await client.query({
+        query: SEARCH_TASK,
+        variables: {
+          query: query,
+          first: data.length ? data.length + 10 : 10
+        }
+      });
+    }catch(err){
+      console.error(err)
+    };
+    const repository_issues_search_result = searchResult.data.search.edges.map(edge => edge.node)
+    console.log('data length', data.length)
+    setData(repository_issues_search_result);
+  };
+
+  const checkIfHasMore = async () => {
+    const labels = searchData.labels ? "label:" + searchData.labels : '';
+    const query = "repo:Hsin1025/dcard_homework " + labels + " in:body " + searchData.body
+    try {
+      var totalIssue = await client.query({
+        query: ISSUE_TOTAL_COUNT,
+        variables: {
+          query: query
+        }
+      });
+    } catch(err){
+      console.error(err)
+    }
+    
+    const total_issue_count = totalIssue.data.search.issueCount;
+    console.log('total', total_issue_count);
+   
+    if(data.length >= total_issue_count){
+      setHasMore(false);
+    }else{
+      setHasMore(true);
+    }
+    return hasMore;
+  }
 
   return (
     <div className={styles.container}>
@@ -21,28 +120,63 @@ export default function Home({ repository_issues }) {
       <Header />
       <main className={styles.main}>
         <div>
-        {loading && <div className={styles.title}>Loading...</div>}
+        {loading && <div>Loading...</div>}
         {
           session &&
           <>
-          <div>
-            {repository_issues.map(item => {
-              return(
-                <div className={styles.card}>
-                  {item.title}
-                </div>
-              )
-            })}
-          </div>
+            <SearchBar 
+              searchData={searchData} 
+              setSearchData={setSearchData} 
+              setData={setData}
+              hasMore={checkIfHasMore()}
+            />
+            <div className='flex justify-end'>
+              <button 
+                onClick={() => setIsOpen(true)} 
+                className='mt-3 p-2 bg-slate-200 rounded-md dark:bg-transparent dark:border' 
+                type='button'
+              >
+                create issue
+              </button>
+            </div>
+            <Modal isOpen={isOpen} setIsOpen={setIsOpen} modal={modal}/>
+            <div>
+              <InfiniteScroll
+                dataLength={data.length}
+                next={getMoreData}
+                hasMore={hasMore}
+                loader={<h3>Loading...</h3>}
+                endMessage={<h4>Nothing more to show</h4>}
+              >
+              {data.map((item) => {
+                return(
+                  <Link key={item.id} href={`/tasks/${item.number}`}>
+                    {item.labels.edges.map(edge => edge.node).map(label => {
+                      return (
+                        <>
+                          <div key={label.id} className={styles.card}>
+                            <p className='text-[#6E798C] dark:text-gray-200 text-xl'>label: {label.name}</p>
+                            <p className='text-[#081F32] dark:text-white text-4xl font-serif'>{item.title}</p>
+                            <p className='text-[#374A59] dark:text-gray-100 text-lg truncate...'>{item.body}</p>
+                          </div>
+                        </>
+                      )
+                    })}
+                  </Link>
+                )
+              })}
+              </InfiniteScroll> 
+            </div>
           </>
         }
         {
           !session &&
           <>
-          <div className='h-screen grid place-content-center'>
-            <p className='text-center text-2xl font-semibold'>Sign In To Continue</p>
-            <img src='/github_cat.svg'/>
-          </div>
+            <div className='h-screen grid place-content-center'>
+              <p className='text-center text-2xl font-semibold'>
+                Sign In To Continue <br />
+              </p>
+            </div>
           </>
         }
         </div>
@@ -62,56 +196,4 @@ export default function Home({ repository_issues }) {
       </footer>
     </div>
   )
-}
-
-export async function getStaticProps() {
-
-  const httpLink = createHttpLink({
-    uri: 'https://api.github.com/graphql'
-  });
-  
-  const authLink = setContext((_, { headers }) => {
-    return {
-      headers: {
-        ...headers,
-        authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`
-      }
-    }
-  })
-
-  const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache()
-  })
-
-  const { data } = await client.query({
-    query: gql`
-    {
-      user(login: "Hsin1025") {
-        repository(name: "dcard_homework") {
-          issues(first: 10, orderBy: {field: CREATED_AT, direction: DESC}) {
-            edges {
-              node {
-                state
-                title
-                body
-              }
-            }
-          }
-        }
-      }
-    }
-    `
-  });
-  
-  const { user } = data;
-  const repository_issues = user.repository.issues.edges.map(edge => edge.node)
-  
-  console.log('repository_issue', repository_issues)
-
-  return {
-    props: {
-      repository_issues
-    }
-  }
 }
---
+++ src/pages/tasks/[taskSlug].tsx
@@ -0,0 +1,162 @@
+import Head from 'next/head';
+import styles from '../../styles/Home.module.css'
+import { gql } from '@apollo/client';
+import { getApolloClient } from '../../../apollo-client';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import Modal from '../../components/Modal';
+import Image from 'next/image';
+import Header from '../../components/Header';
+import { useSession } from 'next-auth/react';
+
+interface Task {
+  task: any, 
+  label: any,
+}
+
+const DELETE_TASK = gql`
+  mutation MyMutation($issueId: ID!, $id: ID!) {
+    updateIssue(input: {id: $id, state: CLOSED}) {
+      clientMutationId
+    }
+    deleteIssue(input: {issueId: $issueId}) {
+      clientMutationId
+    }
+  }
+`
+
+const TASK_SLUG = gql`
+query TaskSlug($number: Int!) {
+  user(login: "Hsin1025") {
+    repository(name: "dcard_homework") {
+      issue(number: $number) {
+        body
+        state
+        title
+        number
+        id
+        labels(first: 1) {
+          nodes {
+            name
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+export default function Task() {
+  const { data: session } = useSession();
+
+  let [isOpen, setIsOpen] = useState(false);
+  let [singleTask, setSingleTask] = useState<Task>({
+    task: [],
+    label: ''
+  });
+
+  let modal = 'update';
+  const client = getApolloClient();
+
+  const router = useRouter();
+  const {taskSlug}: any = router.query
+
+  const reRoute = () => {
+    setTimeout(() => {
+      router.push('/');
+    }, 900)
+  };
+
+  useEffect(() => {
+    getTaskSlug();
+  });
+
+  const getTaskSlug = async () => {
+    const taskData = await client.query({
+      query: TASK_SLUG,
+      variables: {
+        number: parseInt(taskSlug)
+      }
+    });
+    const taskResult = taskData.data.user.repository.issue;
+    const labelResult = taskData.data.user.repository.issue.labels.nodes[0];
+
+    setSingleTask({
+      task: taskResult,
+      label: labelResult
+    });
+  }
+
+  return(
+    <div>
+      <Head>
+        <title>Task Page</title>
+      </Head>
+      <Header />
+      <main className='w-screen h-screen grid dark:bg-black'>
+        {
+          session &&
+          <>
+            <div className='border-gray-500 rounded-md border place-self-center pb-5'>
+              <div className='bg-[#24292F] px-52 h-10 rounded-t-md flex self-center'>
+                <Image 
+                  src='/github_white.svg' 
+                  alt='Github Logo' 
+                  width={35} 
+                  height={35} 
+                  className='w-auto'
+                />
+              </div>
+              <div className='flex justify-start space-x-3 px-3 pt-3'>
+                <h1 className='font-semibold text-3xl'>#{singleTask.task.title}</h1>
+                <button 
+                  onClick={(e) => {
+                    e.preventDefault();
+                  }}
+                  className='inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2'
+                >
+                  {singleTask.label.name}
+                </button>
+              </div>
+              <div className='max-w-[70vh] px-3'>{singleTask.task.body}</div>
+              <div className='flex justify-end space-x-3 px-3'>
+                <button 
+                  onClick={(e) => {
+                    e.preventDefault();
+                    client
+                      .mutate({
+                        mutation: DELETE_TASK,
+                        variables: {
+                          issueId: singleTask.task.id,
+                          id: singleTask.task.id
+                        }
+                      });
+                    reRoute();
+                  }}
+                  className='inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2'
+                >
+                  Delete
+                </button>
+                <button onClick={() => setIsOpen(true)} className={styles.btn}>
+                  Update
+                </button>
+                <Modal isOpen={isOpen} setIsOpen={setIsOpen} singleTask={singleTask} modal={modal}/>
+              </div>
+            </div>
+          </>
+        }
+        {
+          !session &&
+          <>
+            <div className='h-screen grid place-content-center'>
+              <p className='text-center text-2xl font-semibold'>
+                Sign In To Continue<br />
+              </p>
+            </div>
+          </>
+        }
+      </main>
+    </div>
+
+  )
+}
