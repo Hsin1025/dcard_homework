3a032e9644ee2848059e77b71517a21f58eb653f
林芯鈴 <116002213+Hsin1025@users.noreply.github.com>
2023-02-18 14:06:16 +0800

    custom 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+export default function Custom404() {
+    return (
+    <div className="h-screen w-screen grid place-content-center">
+      <h1 className='text-xl'>404-Page Not Found</h1>
+      <a href='/' className='text-lg text-center'>Go Back 🔙</a>
+    </div>
+    )
+}
