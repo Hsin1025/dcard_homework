21e10539992c1498e77515a25797998f1145e7f5
林芯鈴 <116002213+Hsin1025@users.noreply.github.com>
2023-02-28 17:02:06 +0800

    404 and 500 page

--- src/pages/500.js
+++ src/pages/500.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 
-export default function Custom500() {
+export default function Custom404() {
     return (
     <div className="h-screen w-screen grid place-content-center">
       <div className="flex">
@@ -12,7 +12,7 @@ export default function Custom500() {
           alt='Github Logo' 
           className='mr-2'
         />
-        <h1 className="text-xl">404 - Page Not Found</h1>
+        <h1 className="text-xl">500 - Server-side error occurred</h1>
       </div>
       <Link href="/" className="text-lg text-center">Home</Link>
     </div>
